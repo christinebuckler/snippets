26d13e501b4610fd5dc7b29d0b0e493d3880aa71
Christine <bucklerchica@gmail.com>
2018-05-18 10:42:33 -0600

    Update new_computer_installs.sh

--- new_computer_installs.sh
+++ new_computer_installs.sh
@@ -2,6 +2,7 @@ xcode-select --install
 
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew doctor # after macOS or xcode update
+brew tap caskroom/cask
 
 brew install python # https://docs.brew.sh/Homebrew-and-Python
 brew install python3
@@ -15,8 +16,7 @@ brew install awscli
 brew install aws-shell  # accepts the same commands as the AWS CLI, except you don't need to provide the aws prefix.
 brew install node # for awscreds
 npm install -g https://mvnrepo.nordstrom.net/nexus/content/groups/NPMgroup/awscreds/-/awscreds-1.1.1.tgz
-brew tap caskroom/cask
-brew cask install docker  # UI version
+brew cask install docker
 brew cask install iterm2
 brew install bash-completion
 brew cask install sqlworkbenchj
@@ -30,10 +30,8 @@ brew cask install atom
 # apm install merge-conflicts   # to resolve merge conflicts in git
 # apm install remote-sync       # https://atom.io/packages/remote-sync
 brew cask install google-chrome
-brew cask install chrome-devtools
 brew cask install flux
 brew cask install slack
-brew cask install java
 sudo periodic daily weekly monthly  # runs periodic checks on command
 
 # brew install tree
@@ -44,11 +42,12 @@ sudo periodic daily weekly monthly  # runs periodic checks on command
 # brew cask install adobe-acrobat-reader
 # brew cask install blue-jeans # use in browser
 
+# brew cask install java
+# Note: If using `r` and `rJava` the `brew cask` version of java will not work.
+# You want to get rid of that and install the JDK version of java 8 from:
+# http://www.oracle.com/technetwork/java/javase/downloads/jdk8-downloads-2133151.html
 # brew install r
 # brew install r-app
 # brew cask install rstudio
 # brew cask install xquartz     # better graphics in r
 # apm install language-r        # atom plugin for .R scripts
-# Note: If using `r` and `rJava` the `brew cask` version of java will not work.
-# You want to get rid of that and install the JDK version of java 8 from:
-# http://www.oracle.com/technetwork/java/javase/downloads/jdk8-downloads-2133151.html
