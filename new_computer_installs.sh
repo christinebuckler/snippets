ae24aef01d7a9f08ca7bcf8854926b103f34c4d8
Christine <bucklerchica@gmail.com>
2018-05-23 15:32:12 -0600

    Update new_computer_installs.sh

--- new_computer_installs.sh
+++ new_computer_installs.sh
@@ -14,8 +14,7 @@ brew install docker
 # brew install docker-compose # tool for defining and running multi-container docker applications
 brew install awscli
 brew install aws-shell  # accepts the same commands as the AWS CLI, except you don't need to provide the aws prefix.
-brew install node # for awscreds
-npm install -g https://mvnrepo.nordstrom.net/nexus/content/groups/NPMgroup/awscreds/-/awscreds-1.1.1.tgz
+brew install node       # for awscreds
 brew cask install docker
 brew cask install iterm2
 brew install bash-completion
@@ -26,13 +25,13 @@ brew cask install atom
 # apm install platformio-ide-terminal   # run scripts in atom, within venv?
 # apm install python-debugger
 # apm install data-atom         # Query and manage databases from within Atom to run SQL files.
-#                               # To connect to a database, type Alt-Shift-R. Type your script in Atom, then either click Execute or F5.
+                                # To connect to a database, type Alt-Shift-R. Type your script in Atom, then either click Execute or F5.
 # apm install merge-conflicts   # to resolve merge conflicts in git
 # apm install remote-sync       # https://atom.io/packages/remote-sync
 brew cask install google-chrome
 brew cask install flux
 brew cask install slack
-sudo periodic daily weekly monthly  # runs periodic checks on command
+sudo periodic daily weekly monthly
 
 # brew install tree
 # brew install htop
